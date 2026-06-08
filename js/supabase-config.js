2186f281459929daf944d92fc7ef40639168d5e3
Ahmada <ahmada.syafa@gmail.com>
2026-06-08 14:01:50 +0700

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,8 +4,8 @@
 // Dapatkan di: Supabase Dashboard → Settings → API
 // ============================================================
 
-const SUPABASE_URL = 'https://XXXXXXXXXXXXXXXX.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.GANTI_DENGAN_ANON_KEY_ANDA'
+const SUPABASE_URL = 'https://nqdvecessynsovpatkax.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xZHZlY2Vzc3luc292cGF0a2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NTM5NjEsImV4cCI6MjA5NjQyOTk2MX0.S9ZoQqo2m6j7M3m2s1xhvuII3-lAJucOc68yF_Iocac'
 
 const HARGA = { standar: 69000, premium: 120000 }
 const CS    = { wa: '628123456789', email: 'support@invitara.id', jam: 'Senin–Sabtu, 08.00–20.00 WIB' }
